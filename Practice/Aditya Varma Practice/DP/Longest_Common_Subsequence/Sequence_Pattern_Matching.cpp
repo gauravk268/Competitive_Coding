d117eacb397ab2ff661106968cf6d49f1d2aae75
gauravk268 <gauravk26800@gmail.com>
2020-09-08 20:18:47 +0530

    completed practice of LCS and its variation by Aditya Varma Youtuber
    added new solutions:
    1. Longest_Repeating_Subsequence
    2. Sequence_Pattern_Matching
    3. Print_Shortest_Common_SuperSequence
    4. Minimum_Number_of_Insertion_in_String_to_make_Palindrome

---
+++ Practice/Aditya Varma Practice/DP/Longest_Common_Subsequence/Sequence_Pattern_Matching.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll     long long
+#define endl   "\n"
+#define MOD    1e9+7
+#define deb(x) cout<<#x<<"="<<x<<endl
+
+#define f  first
+#define s  second
+#define pb push_back
+#define mp make_pair
+
+typedef vector<int> vi;
+typedef deque<int>  di;
+typedef stack<int>  si;
+typedef priority_queue<int> max_heap;
+typedef priority_queue<int, vector<int>, greater<int>> min_heap;
+typedef pair<int, int> 		 pi;
+typedef tuple<int, int, int> ti;
+
+void printVector(vector<int> &value){ for(auto temp:value){ cout<<temp<<" "; } cout<<"\n";}
+
+// #define ENABLE_DEBUG
+
+
+void sequencePatternMatching(string a, string b, int m, int n)
+{
+	int dp[m+1][n+1];
+
+	for(int i=0; i<=m; i++)
+	{
+		for(int j=0; j<=n; j++)
+		{
+			if(i==0 || j==0)		
+				dp[i][j]=0;
+			else if(a[i-1]==b[j-1])	
+				dp[i][j]=1+dp[i-1][j-1];
+			else 					
+				dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+		}
+	}
+
+	if(min(m, n)==dp[m][n])		cout<<"Yes\n";
+	else	cout<<"No\n";
+	
+	return;
+}
+
+
+void solve()
+{	
+	string a="AXY", b="ADXCPY";		//ABD
+	
+	int m=a.length(), n=b.length();
+	sequencePatternMatching(a, b, m, n);
+	
+    return;
+}
+
+int main() 
+{
+	#ifdef ENABLE_DEBUG
+		freopen("input.txt" , "r", stdin );
+		freopen("output.txt", "w", stdout);
+	#endif
+
+	int t=1;
+	// cin>>t;
+
+	while(t--)
+	{
+		cout<<"Result: \n";
+	    solve();
+	}
+	
+	return 0;
+}
