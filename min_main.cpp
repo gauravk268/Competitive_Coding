0c5b320367bd61d92691a5e1cb734b55a28cd281
Gaurav Kumar <gauravk26800@gmail.com>
2021-01-22 02:18:55 +0530

    added problems

---
+++ min_main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define endl "\n"
+#define dotl " "
+#define MOD 1000000007
+#define deb(x) cout << #x << "=" << x << endl
+#define showRunTime cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+#define ONLINE_JUDGE freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+
+#define f first
+#define s second
+#define pb push_back
+#define pii pair<int, int>
+#define mp make_pair
+#define fr(i,n)	  for(i=0;i<n;i++)
+#define fr1(i,n)	for(i=1;i<=n;i++)
+
+void solution(){
+
+}
+
+int main(){
+  ONLINE_JUDGE;
+
+  std::ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  cin >> t;
+
+  while (t--)
+  {
+    solution();
+  }
+
+  showRunTime;
+  return 0;
+}
+
+/*
+
+
+
+*/
