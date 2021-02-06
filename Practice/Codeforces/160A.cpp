8cef18cfbcc5b93626a7175e36187ccbc9d5abb5
Gaurav Kumar <gauravk26800@gmail.com>
2021-02-07 02:25:59 +0530

    added solved problems in practice

---
+++ Practice/Codeforces/160A.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+
+#define f first
+#define s second
+#define sz(x) (int)x.size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define rsz resize
+#define bk back()
+#define pb push_back
+#define endl "\n"
+#define dotl " "
+
+#define fr1(i,a,b) for(int i = (a); i <= (b); ++i)
+#define fr(i,a) fr1(i,0,a)
+#define rf1(i,a,b) for(int i = (b)-1; i >= (a); --i)
+#define rf(i,a) rf1(i,0,a)
+#define trav(x,a) for (auto& x: a)
+
+#define deb(x) cout << #x << "=" << x << endl
+#define showRunTime cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+#define ONLINE_JUDGE freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+
+const int MX  = 2e5+5;
+const int N   = 1e5+5;
+const int MOD = 1e9+7;      // used in most problems
+const ld  PI  = acos(-1);   // inverse cos of -1
+
+void solution(){
+  int n;
+  cin >> n;
+
+  vi coin(n);
+  int sum = 0;
+  for (int i = 0; i < n; i++){
+    cin >> coin[i];
+    sum += coin[i];
+  }
+
+  sort(coin.begin(), coin.end(), greater<int>());
+  int temp = 0;
+  int i = -1;
+  while(sum >= 2*temp){
+    i++;
+    temp += coin[i];
+  }
+
+  cout << i+1 << endl;
+}
+
+int main(){
+  // ONLINE_JUDGE;
+
+  std::ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  // cin >> t;
+
+  while (t--)
+  {
+    solution();
+  }
+
+  showRunTime;
+  return 0;
+}
+
+/*
+
+2
+2
+3 3
+3
+2 1 2
+
+2
+2
+*/
