4b2fe119d0490a507d2907cc4bbaa1018ff77e62
Gaurav Kumar <gauravk26800@gmail.com>
2021-02-10 16:04:01 +0530

    added more codeforces solution

---
+++ Practice/Codeforces/479C.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+
+#define f first
+#define s second
+#define sz(x) (int)x.size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define rsz resize
+#define bk back()
+#define pb push_back
+#define endl "\n"
+#define dotl " "
+
+#define fr1(i,a,b) for(int i = (a); i <= (b); ++i)
+#define fr(i,a) fr1(i,0,a)
+#define rf1(i,a,b) for(int i = (b)-1; i >= (a); --i)
+#define rf(i,a) rf1(i,0,a)
+#define trav(x,a) for (auto& x: a)
+
+#define deb(x) cout << #x << "=" << x << endl
+#define showRunTime cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+#define ONLINE_JUDGE freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+
+const int MX  = 2e5+5;
+const int N   = 1e5+5;
+const int MOD = 1e9+7;      // used in most problems
+const ld  PI  = acos(-1);   // inverse cos of -1
+
+void solution(){
+  int n;
+  cin >> n;
+
+  priority_queue<pi, vpi, greater<pi>> minH;
+
+  while(n--){
+    int a, b;
+    cin >> a >> b;
+    minH.push(make_pair(a, b));
+  }
+
+  int last=minH.top().s;
+  minH.pop();
+  while(!minH.empty()){
+    pi c = minH.top();
+    minH.pop();
+
+    if(c.s >= last){
+      last = c.s;
+    }
+    else{
+      last = c.f;
+    }
+  }
+
+  cout << last << endl;
+}
+
+int main(){
+  std::ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t = 1;
+  // ONLINE_JUDGE;
+  // cin >> t;
+
+  while (t--)
+  {
+    solution();
+  }
+
+  showRunTime;
+  return 0;
+}
+
+/*
+
+2
+3
+5 2
+3 1
+4 2
+3
+6 1
+5 2
+4 3
+
+2
+6
+
+*/
