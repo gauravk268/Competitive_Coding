a5125b3bc580381cdc15fe05022bf5767ed8ba6d
Gaurav Kumar <gauravk26800@gmail.com>
2021-02-17 14:06:08 +0530

    added 2 codeforces solution

---
+++ Practice/Codeforces/1490D.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+
+#define f first
+#define s second
+#define sz(x) (int)x.size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define rsz resize
+#define bk back()
+#define pb push_back
+#define endl "\n"
+#define dotl " "
+
+#define fr1(i,a,b) for(int i = (a); i <= (b); ++i)
+#define fr(i,a) fr1(i,0,a)
+#define rf1(i,a,b) for(int i = (b)-1; i >= (a); --i)
+#define rf(i,a) rf1(i,0,a)
+#define trav(x,a) for (auto& x: a)
+
+#define deb(x) cout << #x << "=" << x << endl
+#define showRunTime cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+#define ONLINE_JUDGE freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+
+const int MX  = 2e5+5;
+const int N   = 1e5+5;
+const int MOD = 1e9+7;      // used in most problems
+const ld  PI  = acos(-1);   // inverse cos of -1
+
+vi adj(200), res(200);
+int n;
+void fun(int indexS, int indexE, int d){
+  if (indexS > indexE || indexS<1 || indexE > n)
+  {
+    return;
+  }
+
+  if(indexS == indexE){
+    res[indexS] = d;
+    return;
+  }
+
+  // auto maxValue = max_element( adj.begin()+indexS, adj.begin()+indexE-1 );
+  // int index = maxValue - adj.begin();
+  // res[index] = d;
+
+  int index, ele=0;
+  for (int i = indexS; i <= indexE; i++){
+    if(adj[i] > ele){
+      index = i;
+      ele = adj[i];
+    }
+  
+  res[index] = d;
+
+  fun(indexS, index - 1, d + 1);
+  fun(index+1, indexE, d+1);
+  return;
+}
+
+void solution(){
+  cin >> n;
+
+  for (int i = 1; i <= n; i++){
+    cin >> adj[i];
+  }
+
+  fun(1, n, 0);
+
+  for (int i = 1; i <= n; i++){
+    cout << res[i] << " ";
+  }
+  cout << endl;
+}
+
+int main(){
+  std::ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int t = 1;
+  // ONLINE_JUDGE;
+  cin >> t;
+
+  while (t--)
+  {
+    solution();
+  }
+
+  showRunTime;
+  return 0;
+}
+
+/*
+
+3
+5
+3 5 2 1 4
+1
+1
+4
+4 3 1 2
+
+1 0 2 3 1 
+0 
+0 1 3 2
+
+*/
