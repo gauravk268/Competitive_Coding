2a3bdfa9d57cdae9c5141391668d17af0a1388be
Gaurav Kumar <gauravk26800@gmail.com>
2021-02-23 22:29:40 +0530

    added coding problems solved today

---
+++ Practice/CodeChef/Mussadi_Lal_and_Stairs.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+
+#define f first
+#define s second
+#define sz(x) (int)x.size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define rsz resize
+#define bk back()
+#define pb push_back
+#define endl "\n"
+#define dotl " "
+
+#define fr1(i,a,b) for(int i = (a); i <= (b); ++i)
+#define fr(i,a) fr1(i,0,a)
+#define rf1(i,a,b) for(int i = (b)-1; i >= (a); --i)
+#define rf(i,a) rf1(i,0,a)
+#define trav(x,a) for (auto& x: a)
+
+#define deb(x) cout << #x << "=" << x << endl
+#define showRunTime cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+#define ONLINE_JUDGE freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
+
+const int MX  = 2e5+5;
+const int N   = 1e5+5;
+const int MOD = 1e9+7;      // used in most problems
+const ld  PI  = acos(-1);   // inverse cos of -1
+
+ll dp[N];
+void dpFun(){
+  for (int i = 0; i<N; i++){
+    if(i<3)
+    {
+      dp[i] = i;
+    }
+    else if(i==3)
+    {
+      dp[i] = 4;
+    }
+    else
+    {
+      dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+
+    dp[i] %= MOD;
+  }
+}
+
+ll v[N]={0};
+ll fun(int x){
+  if(v[x])
+  {
+    return v[x];
+  }
+
+  if(x && x <=2)
+  {
+    return x;
+  }
+
+  if(x && x==3)
+  {
+    return 4;
+  }
+
+  v[x] = fun(x - 1) + fun(x - 2) + fun(x - 3);
+  v[x] = v[x] % MOD;
+  return v[x];
+}
+
+void solution(){
+  ll n;
+  cin >> n;
+
+  // cout << fun(n) << endl;
+  cout << dp[n] << endl;
+}
+
+int main(){
+  std::ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  dpFun();
+  int t = 1;
+  // ONLINE_JUDGE;
+  cin >> t;
+
+  while (t--)
+  {
+    solution();
+  }
+
+  showRunTime;
+  return 0;
+}
+
+/*
+
+Input:
+3
+5
+8
+3
+
+Output:
+13
+81
+4
+
+*/
