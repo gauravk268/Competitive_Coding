34654dfef7ef602e976e0a8906e51eef2799164b
Gaurav Kumar <gauravk26800@gmail.com>
2021-02-27 21:55:21 +0530

    updated problem

--- Practice/Codeforces/268C.cpp
+++ Practice/Codeforces/268C.cpp
@@ -20,33 +20,36 @@ typedef vector<pi> vpi;
 #define endl "\n"
 #define dotl " "
 
-#define fr1(i,a,b) for(int i = (a); i <= (b); ++i)
-#define fr(i,a) fr1(i,0,a)
-#define rf1(i,a,b) for(int i = (b)-1; i >= (a); --i)
-#define rf(i,a) rf1(i,0,a)
-#define trav(x,a) for (auto& x: a)
+#define fr1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define fr(i, a) fr1(i, 0, a)
+#define rf1(i, a, b) for (int i = (b)-1; i >= (a); --i)
+#define rf(i, a) rf1(i, 0, a)
+#define trav(x, a) for (auto &x : a)
 
 #define deb(x) cout << #x << "=" << x << endl
 #define showRunTime cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
 #define ONLINE_JUDGE freopen("input.txt", "r", stdin);
 // freopen("output.txt", "w", stdout);
 
-const int MX  = 2e5+5;
-const int N   = 1e5+5;
-const int MOD = 1e9+7;      // used in most problems
-const ld  PI  = acos(-1);   // inverse cos of -1
+const int MX = 2e5 + 5;
+const int N = 1e5 + 5;
+const int MOD = 1e9 + 7; // used in most problems
+const ld PI = acos(-1);  // inverse cos of -1
 
-void solution(){
+void solution()
+{
   int n, m;
   cin >> n >> m;
 
-  cout << min(n, m)+1 << endl;
-  for (int i = 0; i <= min(n, m); i++){
+  cout << min(n, m) + 1 << endl;
+  for (int i = 0; i <= min(n, m); i++)
+  {
     cout << i << " " << min(n, m) - i << endl;
   }
 }
 
-int main(){
+int main()
+{
   std::ios::sync_with_stdio(false);
   cin.tie(0);
 
