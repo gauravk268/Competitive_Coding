32a1aceec49b1e6c661b43e175d3f1751f2759fa
Shreyas Tandon <60067261+standon512@users.noreply.github.com>
2021-10-07 22:31:35 +0530

    Added leetcode solutions

---
+++ LeetCode/48. Rotate Image.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+       int n=matrix[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+                swap(matrix[i][j],matrix[j][i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n/2;j++)
+            {
+                
+                    swap(matrix[i][j],matrix[i][n-j-1]);
+            }
+        }
+        
+    }
+};
