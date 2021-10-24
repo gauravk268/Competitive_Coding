7dc27ebb31f559375752beda9ae8c794ab71f931
codingNightmare22 <91601032+codingNightmare22@users.noreply.github.com>
2021-10-24 12:54:35 +0530

    Create 374. Guess Number Higher or Lower.java
    Adding solution of leetcode Q. 374

---
+++ LeetCode/374. Guess Number Higher or Lower.java
@@ -0,0 +1,28 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    
+    public int guessNumber(int n) {
+        int start=1, end = n;
+        while(true){
+            int mid = start + (end - start)/2;
+            int res = guess(mid);
+            if(res == 0){
+                return mid;
+            }
+            else if(res == -1){
+                end  = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+    }
+}
