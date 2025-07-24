8bab2d693542212a673bde0c215b426369149a92
Gaurav Kumar <gauravk26800@gmail.com>
2025-07-24 23:38:10 +0530

    [287] [Time Beats: 88.03%] [Memory Beats: 48.72%] - LeetPush

---
+++ LeetCode/Find-the-Duplicate-Number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0], fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
