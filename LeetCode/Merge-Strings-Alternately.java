6758acb87a487d60de85999c51efff6fc848308b
Gaurav Kumar <gauravk26800@gmail.com>
2025-12-29 00:05:38 +0530

    [1768] [Time Beats: 94.34%] [Memory Beats: 70.99%] - LeetPush

--- LeetCode/Merge-Strings-Alternately.java
+++ LeetCode/Merge-Strings-Alternately.java
@@ -1,21 +1,17 @@
 1class Solution {
 2    public String mergeAlternately(String word1, String word2) {
-3        String result = new String();
-4        int i=0, j=0;
-5        while(i<word1.length() || j<word2.length()){
-6            if(result.length()%2 == 0 && i<word1.length()){
-7                result = result + word1.charAt(i++);
-8            }
-9            else if(result.length()%2 == 1 && j<word2.length()){
-10                result = result + word2.charAt(j++);
-11            }
-12            else if(i<word1.length()){
-13                result = result + word1.charAt(i++);
-14            }
-15            else {
-16                result = result + word2.charAt(j++);
-17            }
-18        }
-19        return result;
-20    }
-21}
+3        int i = 0, j = 0;
+4        int n=word1.length(), m=word2.length();
+5        StringBuilder result = new StringBuilder(n+m);
+6
+7        while (i < n || j < m) {
+8            if (j >= m || result.length() % 2 == 0 && i < n) {
+9                result.append(word1.charAt(i++));
+10            } else if (i >= n || result.length() % 2 == 1 && j < m) {
+11                result.append(word2.charAt(j++));
+12            }
+13        }
+14
+15        return result.toString();
+16    }
+17}
