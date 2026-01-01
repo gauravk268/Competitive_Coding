0c5624ae4aacc26fb0593ecf2df0a1a8770e4f4d
Gaurav Kumar <gauravk26800@gmail.com>
2026-01-01 22:00:14 +0530

    [345] [Time Beats: 82.76%] [Memory Beats: 27.91%] - LeetPush

--- LeetCode/Reverse-Vowels-of-a-String.java
+++ LeetCode/Reverse-Vowels-of-a-String.java
@@ -1,28 +1,25 @@
 1class Solution {
-2    public Boolean isVowel(char s){
-3        return List.of('a', 'e', 'i', 'o', 'u').contains(Character.toLowerCase(s));
-4    }
-5
-6    public String reverseVowels(String s) {
-7        Queue<Integer> q=new LinkedList<>();
-8        int n=s.length();
-9        StringBuilder sb = new StringBuilder(s);
-10
-11        int j=n-1;
-12        for(int i=0; i<n; i++){
-13            if(isVowel(s.charAt(i))){
-14                while(j>=i && !isVowel(s.charAt(j))){
-15                    j--;
-16                }
-17
-18                if(j>i){
-19                    sb.setCharAt(i, s.charAt(j));
-20                    sb.setCharAt(j, s.charAt(i));
-21                    j--;
-22                }
-23            }
-24        }
-25
-26        return sb.toString();
-27    }
-28}
+2    public String reverseVowels(String s) {
+3        int n=s.length();
+4        char[] sb = s.toCharArray();
+5        String vow = "aeiouAEIOU";
+6
+7        int i=0, j=n-1;
+8        while(i<j){
+9            while(i<j && vow.indexOf(sb[i])==-1){
+10                i++;
+11            }
+12            while(i<j && vow.indexOf(sb[j])==-1){
+13                j--;
+14            }
+15
+16            char temp = sb[i];
+17            sb[i]=sb[j];
+18            sb[j]=temp;
+19            i++;
+20            j--;
+21        }
+22
+23        return new String(sb);
+24    }
+25}
