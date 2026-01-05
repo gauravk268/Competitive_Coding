7d3201bcd0b67b0db7fe2999644800ac86e29de4
Gaurav Kumar <gauravk26800@gmail.com>
2026-01-05 22:39:11 +0530

    [1493] [Time Beats: 98.81%] [Memory Beats: 67.13%] - LeetPush

---
+++ LeetCode/Longest-Subarray-of-1's-After-Deleting-One-Element.java
@@ -0,0 +1,26 @@
+1class Solution {
+2    public int longestOnes(int[] nums, int k) {
+3        int n = nums.length, len = 0, j = 0, tempK=k;
+4
+5        for (int i = 0; i < n; i++) {
+6            if (nums[i] == 0) {
+7                if (k != 0) {
+8                    k--;
+9                } else {
+10                    while (nums[j] != 0) {
+11                        j++;
+12                    }
+13                    j++;
+14                }
+15            }
+16
+17            len = Math.max(len, i - j + 1);
+18        }
+19
+20        return len-tempK;
+21    }
+22
+23    public int longestSubarray(int[] nums) {
+24        return longestOnes(nums, 1);
+25    }
+26}
