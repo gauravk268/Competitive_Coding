837451c71f40d740e87c09c6073e09c36d507bc3
Gaurav Kumar <gauravk26800@gmail.com>
2026-01-06 23:55:49 +0530

    [2352] [Time Beats: 51.66%] [Memory Beats: 16.53%] - LeetPush

--- LeetCode/Equal-Row-and-Column-Pairs.java
+++ LeetCode/Equal-Row-and-Column-Pairs.java
@@ -1,25 +1,26 @@
 1class Solution {
-2    public int equalPairs(int[][] g) {
-3        int n = g.length;
+2    public int equalPairs(int[][] grid) {
+3        int n = grid.length;
 4
-5        Map<List<Integer>, Integer> mp = new HashMap<>(), mp1 = new HashMap<>();
-6        for (int i = 0; i < n; i++) {
-7            List<Integer> temp = new ArrayList<>(), temp1 = new ArrayList<>();
-8            for (int j = 0; j < n; j++) {
-9                temp.add(g[i][j]);
-10                temp1.add(g[j][i]);
-11            }
-12            mp.put(temp, mp.getOrDefault(temp, 0) + 1);
-13            mp1.put(temp1, mp1.getOrDefault(temp1, 0) + 1);
-14        }
-15
-16        int result = 0;
-17        for (List<Integer> key : mp.keySet()) {
-18            if (mp1.containsKey(key)) {
-19                result += (mp1.get(key) * mp.get(key));
+5        Map<String, Integer> mp = new HashMap<>();
+6        for (int[] a : grid) {
+7            StringBuilder sb = new StringBuilder();
+8            for (int b : a) {
+9                sb.append(b).append(",");
+10            }
+11            String val = sb.toString();
+12            mp.put(val, mp.getOrDefault(val, 0) + 1);
+13        }
+14
+15        int result = 0;
+16        for (int i = 0; i < n; i++) {
+17            StringBuilder sb = new StringBuilder();
+18            for (int j = 0; j < n; j++) {
+19                sb.append(grid[j][i]).append(",");
 20            }
-21        }
-22
-23        return result;
-24    }
-25}
+21            result += mp.getOrDefault(sb.toString(), 0);
+22        }
+23
+24        return result;
+25    }
+26}
