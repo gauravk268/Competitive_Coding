c214532d38ce1e97aa48d3bb3688694e3a8c389e
Gaurav Kumar <gauravk26800@gmail.com>
2026-01-06 23:43:41 +0530

    [1207] [Time Beats: 97.88%] [Memory Beats: 18.32%] - LeetPush

--- LeetCode/Unique-Number-of-Occurrences.java
+++ LeetCode/Unique-Number-of-Occurrences.java
@@ -2,18 +2,17 @@
 2    public boolean uniqueOccurrences(int[] arr) {
 3        Map<Integer, Integer> mp = new HashMap<>();
 4
-5        for (int i = 0; i < arr.length; i++) {
-6            mp.put(arr[i], mp.getOrDefault(arr[i], 0) + 1);
+5        for (int i : arr) {
+6            mp.put(i, mp.getOrDefault(i, 0) + 1);
 7        }
 8
-9        int[] pos = new int[1001];
-10        for(Integer key:mp.keySet()){
-11            if (pos[mp.get(key)] == 1) {
+9        Set<Integer> s = new HashSet<>();
+10        for (Integer i : mp.values()) {
+11            if (!s.add(i)) {
 12                return false;
 13            }
-14            pos[mp.get(key)] = 1;
-15        }
-16
-17        return true;
-18    }
-19}
+14        }
+15
+16        return true;
+17    }
+18}
