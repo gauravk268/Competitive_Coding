4cb4dfed53271e236f545bf035596427991d91ae
Gaurav Kumar <gauravk26800@gmail.com>
2026-01-06 23:48:56 +0530

    [1657] [Time Beats: 60.25%] [Memory Beats: 38.83%] - LeetPush

--- LeetCode/Determine-if-Two-Strings-Are-Close.java
+++ LeetCode/Determine-if-Two-Strings-Are-Close.java
@@ -1,36 +1,27 @@
 1class Solution {
 2    public boolean closeStrings(String word1, String word2) {
-3        int n = word1.length(), m = word2.length();
-4
-5        if (n != m)
-6            return false;
+3        if (word1.length() != word2.length())
+4            return false;
+5
+6        int[] a = new int[26], b = new int[26];
 7
-8        Set<Character> st = new HashSet<>();
-9        int[] fa = new int[26], fb = new int[26];
-10        for (int i = 0; i < 26; i++) {
-11            fa[i] = 0;
-12            fb[i] = 0;
-13        }
-14        for (char i : word1.toCharArray()) {
-15            st.add(i);
-16            fa[i - 'a']++;
-17        }
-18
-19        for (char i : word2.toCharArray()) {
-20            if (st.add(i)) {
-21                return false;
-22            }
-23            fb[i - 'a']++;
-24        }
-25
-26        Arrays.sort(fa);
-27        Arrays.sort(fb);
-28
-29        for (int i = 0; i < 26; i++) {
-30            if (fa[i] != fb[i])
-31                return false;
-32        }
-33
-34        return true;
-35    }
-36}
+8        for (char c : word1.toCharArray()) {
+9            a[c - 'a']++;
+10        }
+11
+12        for (char c : word2.toCharArray()) {
+13            b[c - 'a']++;
+14        }
+15
+16        for (int i = 0; i < 26; i++) {
+17            if ((a[i] == 0 && b[i] != 0) || (a[i] != 0 && b[i] == 0)) {
+18                return false;
+19            }
+20        }
+21
+22        Arrays.sort(a);
+23        Arrays.sort(b);
+24
+25        return Arrays.equals(a, b);
+26    }
+27}
