6515b82fa7ffe5a139db97a74cb01dfadfd910e8
Gaurav Kumar <gauravk26800@gmail.com>
2026-01-10 19:26:51 +0530

    [206] [Time Beats: 100.00%] [Memory Beats: 70.37%] - LeetPush

--- LeetCode/Reverse-Linked-List.java
+++ LeetCode/Reverse-Linked-List.java
@@ -1,28 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        if(head == null)    return head;
-        
-        ListNode curr=head, pre=null, temp;
-
-        while(curr != null && curr.next != null){
-            temp = curr;
-            curr = curr.next;
-            temp.next = pre;
-            pre = temp;
-        }
-
-        curr.next = pre;
-
-        return curr;
-    }
-}
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode reverseList(ListNode h) {
+13        if(h==null || h.next == null)   return h;
+14
+15        ListNode p=h, prev=null;
+16        while(p!=null){
+17            ListNode temp = p;
+18            p = p.next;
+19            temp.next = prev;
+20            prev = temp;
+21        }
+22        return prev;
+23    }
+24}
