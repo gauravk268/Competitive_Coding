04dde1e2faa2c4f8f0031ec3795006bf9320d631
Gaurav Kumar <gauravk26800@gmail.com>
2026-01-20 23:24:03 +0530

    [2336] [Time Beats: 89.79%] [Memory Beats: 33.23%] - LeetPush

--- LeetCode/Smallest-Number-in-Infinite-Set.java
+++ LeetCode/Smallest-Number-in-Infinite-Set.java
@@ -1,26 +1,34 @@
 1class SmallestInfiniteSet {
-2    private Queue<Integer> q;
-3
-4    public SmallestInfiniteSet() {
-5        q = new PriorityQueue<>();
-6        for(int i=1; i<=1000; i++){
-7            q.add(i);
-8        }
-9    }
-10    
-11    public int popSmallest() {
-12        return q.poll();
-13    }
-14    
-15    public void addBack(int num) {
-16        if(!q.contains(num))
-17            q.add(num);
-18    }
-19}
-20
-21/**
-22 * Your SmallestInfiniteSet object will be instantiated and called as such:
-23 * SmallestInfiniteSet obj = new SmallestInfiniteSet();
-24 * int param_1 = obj.popSmallest();
-25 * obj.addBack(num);
-26 */
+2    int num=1;
+3    private Queue<Integer> q;
+4    private Set<Integer> s;
+5
+6    public SmallestInfiniteSet() {
+7        q = new PriorityQueue<>();
+8        s = new HashSet<>();
+9        num=1;
+10    }
+11    
+12    public int popSmallest() {
+13        if(!q.isEmpty() && q.peek() < num){
+14            int val=q.poll();
+15            s.remove(val);
+16            return val;
+17        }
+18
+19        return num++;
+20    }
+21    
+22    public void addBack(int val) {
+23        if(val < num && s.add(val)){
+24            q.add(val);
+25        }
+26    }
+27}
+28
+29/**
+30 * Your SmallestInfiniteSet object will be instantiated and called as such:
+31 * SmallestInfiniteSet obj = new SmallestInfiniteSet();
+32 * int param_1 = obj.popSmallest();
+33 * obj.addBack(num);
+34 */
