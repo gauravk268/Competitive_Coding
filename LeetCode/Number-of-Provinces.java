6b68960c8ca16ae1331922ac14b2de57028a3c7c
Gaurav Kumar <gauravk26800@gmail.com>
2026-01-23 23:28:37 +0530

    [547] [Time Beats: 47.91%] [Memory Beats: 32.67%] - LeetPush

--- LeetCode/Number-of-Provinces.java
+++ LeetCode/Number-of-Provinces.java
@@ -2,27 +2,28 @@
 2    public int findCircleNum(int[][] mp) {
 3        int n=mp.length;
 4        boolean[] vis = new boolean[n];
-5        Queue<Integer> q = new LinkedList<>();
-6
-7        int res=0;
-8        for(int i=0; i<n; i++){
-9            if(!vis[i]){
-10                q.add(i);
-11                res++;
-12
-13                while(!q.isEmpty()){
-14                    int curr = q.poll();
-15
-16                    for(int j=0; j<n; j++){
-17                        if(mp[curr][j] == 1 && !vis[j]){
-18                            q.add(j);
-19                            vis[j]=true;
-20                        }
-21                    }
-22                }
-23            }
-24        }
-25
-26        return res;
-27    }
-28}
+5
+6        int res=0;
+7        for(int i=0; i<n; i++){
+8            if(!vis[i]){
+9                vis[i]=true;
+10                res++;
+11                Queue<Integer> q = new LinkedList<>();
+12                q.add(i);
+13
+14                while(!q.isEmpty()){
+15                    int curr = q.poll();
+16
+17                    for(int j=0; j<n; j++){
+18                        if(mp[curr][j] == 1 && !vis[j]){
+19                            q.add(j);
+20                            vis[j]=true;
+21                        }
+22                    }
+23                }
+24            }
+25        }
+26
+27        return res;
+28    }
+29}
