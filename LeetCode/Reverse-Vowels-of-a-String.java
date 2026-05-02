9158268d781f8292225d85b03b7768cee38fec5f
Gaurav Kumar <gauravk26800@gmail.com>
2026-05-02 20:59:52 +0530

    [345] [Time Beats: 39.14%] [Memory Beats: 97.17%] - LeetPush

--- LeetCode/Reverse-Vowels-of-a-String.java
+++ LeetCode/Reverse-Vowels-of-a-String.java
@@ -1,25 +1,32 @@
 1class Solution {
-2    public String reverseVowels(String s) {
-3        int n=s.length();
-4        char[] sb = s.toCharArray();
-5        String vow = "aeiouAEIOU";
-6
-7        int i=0, j=n-1;
-8        while(i<j){
-9            while(i<j && vow.indexOf(sb[i])==-1){
-10                i++;
-11            }
-12            while(i<j && vow.indexOf(sb[j])==-1){
-13                j--;
-14            }
-15
-16            char temp = sb[i];
-17            sb[i]=sb[j];
-18            sb[j]=temp;
-19            i++;
-20            j--;
-21        }
-22
-23        return new String(sb);
-24    }
-25}
+2    public boolean isVowel(char c) {
+3        return ("aeiouAEIOU".indexOf(c) != -1);
+4    }
+5
+6    public String reverseVowels(String s) {
+7        StringBuilder sb = new StringBuilder(s);
+8
+9        int n = s.length();
+10        int i = 0, j = s.length() - 1;
+11
+12        while (i < j) {
+13            while (i < n && !this.isVowel(s.charAt(i))) {
+14                i++;
+15            }
+16
+17            while (j >= 0 && !this.isVowel(s.charAt(j))) {
+18                j--;
+19            }
+20
+21            if(i<n && j>=0){
+22                char c = s.charAt(i);
+23                sb.setCharAt(i, s.charAt(j));
+24                sb.setCharAt(j, c);
+25                i++;
+26                j--;
+27            }
+28        }
+29
+30        return sb.toString();
+31    }
+32}
