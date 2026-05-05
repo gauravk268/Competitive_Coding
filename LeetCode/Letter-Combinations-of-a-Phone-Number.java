5ea2925bd980b7e6c6bd4622814b162b9cdd8a76
Gaurav Kumar <gauravk26800@gmail.com>
2026-05-06 00:06:36 +0530

    [17] [Time Beats: 54.89%] [Memory Beats: 99.22%] - LeetPush

--- LeetCode/Letter-Combinations-of-a-Phone-Number.java
+++ LeetCode/Letter-Combinations-of-a-Phone-Number.java
@@ -1,23 +1,34 @@
 1class Solution {
-2
-3    public List<String> letterCombinations(String digits) {
-4        List<String> mob = List.of("abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz");
-5
-6        List<String> resp = new ArrayList<>();
-7        helper(resp, mob, digits, 0, "");
-8        return resp;
-9    }
-10
-11    private void helper(List<String> resp, List<String> mob, String digits, int i, String res){
-12        if(i >= digits.length()){
-13            resp.add(res);
-14            return;
-15        }
-16
-17        String key = mob.get(digits.charAt(i)-'0'-2);
-18        
-19        for(char c:key.toCharArray()){
-20            helper(resp, mob, digits, i+1, res + c);
-21        }
-22    }
-23}
+2    public static final Map<Character, String> mob = Map.of(
+3            '2', "abc",
+4            '3', "def",
+5            '4', "ghi",
+6            '5', "jkl",
+7            '6', "mno",
+8            '7', "pqrs",
+9            '8', "tuv",
+10            '9', "wxyz");
+11    public int digitLen;
+12    List<String> resp;
+13
+14    public List<String> letterCombinations(String digits) {
+15        digitLen = digits.length();
+16        resp = new ArrayList<>();
+17
+18        helper(digits, 0, new StringBuilder());
+19        return resp;
+20    }
+21
+22    private void helper(String digits, int i, StringBuilder res) {
+23        if (i >= digitLen) {
+24            resp.add(res.toString());
+25            return;
+26        }
+27
+28        for (char c : Solution.mob.get(digits.charAt(i)).toCharArray()) {
+29            helper(digits, i + 1, res.append(c));
+30            res.setLength(res.length() - 1);
+31        }
+32    }
+33}
+34
