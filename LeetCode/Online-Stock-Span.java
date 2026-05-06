7d97320fb9052ad96d4003a5d010ebbb822b57bb
Gaurav Kumar <gauravk26800@gmail.com>
2026-05-06 23:57:39 +0530

    [901] [Time Beats: 45.23%] [Memory Beats: 44.49%] - LeetPush

--- LeetCode/Online-Stock-Span.java
+++ LeetCode/Online-Stock-Span.java
@@ -1,25 +1,22 @@
 1class StockSpanner {
-2    List<Integer> stock;
+2    Stack<int[]> stock;
 3
 4    public StockSpanner() {
-5        stock = new ArrayList<>();
+5        stock = new Stack<>();
 6    }
 7
 8    public int next(int price) {
-9        int result = 1;
-10        for (int i = stock.size() - 1; i >= 0; i--) {
-11            if (stock.get(i) <= price) {
-12                result++;
-13            } else
-14                break;
-15        }
-16        stock.add(price);
-17        return result;
-18    }
-19}
-20
-21/**
-22 * Your StockSpanner object will be instantiated and called as such:
-23 * StockSpanner obj = new StockSpanner();
-24 * int param_1 = obj.next(price);
-25 */
+9        int span = 1;
+10        while(!stock.isEmpty() && stock.peek()[0] <= price){
+11            span+=stock.pop()[1];
+12        }
+13        stock.add(new int[]{price, span});
+14        return span;        
+15    }
+16}
+17
+18/**
+19 * Your StockSpanner object will be instantiated and called as such:
+20 * StockSpanner obj = new StockSpanner();
+21 * int param_1 = obj.next(price);
+22 */
