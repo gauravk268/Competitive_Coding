9361de6311e7d0623a4fd982685ca316125a9b16
Gaurav Kumar <gauravk26800@gmail.com>
2026-07-13 09:16:30 +0530

    [1768] [Time Beats: 96.13%] [Memory Beats: 89.53%] - LeetPush

--- LeetCode/1768-Merge-Strings-Alternately.java
+++ LeetCode/1768-Merge-Strings-Alternately.java
@@ -3,19 +3,14 @@ class Solution {
         StringBuilder sb = new StringBuilder();
 
         int n=word1.length(), m=word2.length();
-        int i=0, j=0;
-        while(i<n && j<m){
-            sb.append(word1.charAt(i++));
-            sb.append(word2.charAt(j++));
+        int i=0;
+        while(i<n && i<m){
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i++));
         }
 
-        while(i<n){
-            sb.append(word1.charAt(i++));
-        }
-
-        while(j<m){
-            sb.append(word2.charAt(j++));
-        }
+        sb.append(word1.substring(i, n));
+        sb.append(word2.substring(i, m));
 
         return sb.toString();
     }
