ca3aafbe67a59e5d400237a1095b9732a0416920
Gaurav Kumar <gauravk26800@gmail.com>
2026-07-14 23:18:39 +0530

    [238] [Time Beats: 25.42%] [Memory Beats: 6.39%] - LeetPush

--- LeetCode/238-Product-of-Array-Except-Self.java
+++ LeetCode/238-Product-of-Array-Except-Self.java
@@ -1,26 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
+        int[] ans = new int[n];
+
+        ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            ans[i] = nums[i - 1] * ans[i - 1];
+        }
 
         int prod = 1;
-        int countZero = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                countZero++;
-            } else {
-                prod = prod * nums[i];
-            }
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = prod * ans[i];
+            prod = prod * nums[i];
         }
 
-        int[] res = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (countZero == 0) {
-                res[i] = prod / nums[i];
-            } else if (countZero == 1 && nums[i] == 0) {
-                res[i] = prod;
-            }
-        }
-
-        return res;
+        return ans;
     }
 }
