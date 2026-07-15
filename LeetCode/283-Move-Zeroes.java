aeb815a36fc9a7672160b0592adb85361d0377d8
Gaurav Kumar <gauravk26800@gmail.com>
2026-07-15 23:33:43 +0530

    [283] [Time Beats: 92.18%] [Memory Beats: 20.33%] - LeetPush

--- LeetCode/283-Move-Zeroes.java
+++ LeetCode/283-Move-Zeroes.java
@@ -2,19 +2,15 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int n = nums.length;
 
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                j = i;
-                while (j < n && nums[j] == 0) {
-                    j++;
-                }
-
-                if (j < n && nums[j] != 0) {
-                    nums[i] = nums[j];
-                    nums[j] = 0;
-                }
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            if (nums[j] != 0) {
+                nums[i++] = nums[j];
             }
+        }
+
+        while (i < n) {
+            nums[i++] = 0;
         }
     }
 }
