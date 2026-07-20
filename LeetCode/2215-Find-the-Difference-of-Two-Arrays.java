b0cb96e314b48bf6b47041f0741eaeb9fa5d57b9
Gaurav Kumar <gauravk26800@gmail.com>
2026-07-20 11:48:42 +0530

    [2215] [Time Beats: 99.45%] [Memory Beats: 74.42%] - LeetPush

--- LeetCode/2215-Find-the-Difference-of-Two-Arrays.java
+++ LeetCode/2215-Find-the-Difference-of-Two-Arrays.java
@@ -2,34 +2,39 @@ class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         int n = nums1.length, m = nums2.length;
 
-        Map<String, Boolean> mp = new HashMap<>();
+        int[] mp1 = new int[2002], mp2 = new int[2002];
 
         int i = 0, j = 0;
         while (i < n || j < m) {
-            if (i < n)
-                mp.put("0_" + Integer.toString(nums1[i++]), true);
-            if (j < m)
-                mp.put("1_" + Integer.toString(nums2[j++]), true);
+            if (i < n) {
+                mp1[(nums1[i] >= 0) ? nums1[i] : 1000 + Math.abs(nums1[i])] = 1;
+                i++;
+            }
+            if (j < m) {
+                mp2[(nums2[j] >= 0) ? nums2[j] : 1000 + Math.abs(nums2[j])] = 1;
+                j++;
+            }
         }
 
-        List<Integer> res1 = new ArrayList<>();
-        List<Integer> res2 = new ArrayList<>();
-
+        List<Integer> res1 = new ArrayList<>(), res2 = new ArrayList<>();
         i = 0;
         j = 0;
         while (i < n || j < m) {
-            if (i < n && !mp.containsKey("1_" + Integer.toString(nums1[i]))) {
-                mp.put("1_" + Integer.toString(nums1[i]), true);
-                res1.add(nums1[i]);
+            if (i < n) {
+                if (mp2[(nums1[i] >= 0) ? nums1[i] : 1000 + Math.abs(nums1[i])] != 1) {
+                    res1.add(nums1[i]);
+                    mp2[(nums1[i] >= 0) ? nums1[i] : 1000 + Math.abs(nums1[i])] = 1;
+                }
+                i++;
             }
 
-            if (j < m && !mp.containsKey("0_" + Integer.toString(nums2[j]))) {
-                mp.put("0_" + Integer.toString(nums2[j]), true);
-                res2.add(nums2[j]);
+            if (j < m) {
+                if (mp1[(nums2[j] >= 0) ? nums2[j] : 1000 + Math.abs(nums2[j])] != 1) {
+                    res2.add(nums2[j]);
+                    mp1[(nums2[j] >= 0) ? nums2[j] : 1000 + Math.abs(nums2[j])] = 1;
+                }
+                j++;
             }
-
-            i++;
-            j++;
         }
 
         return List.of(res1, res2);
